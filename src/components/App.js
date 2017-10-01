28b6219852be3ae732bcef2c400bd785a9a1b261
rogueITman <aelam71@yahoo.com>
2017-09-30 22:23:17 -0500

    refactored addGift fn to reduce code

--- src/components/App.js
+++ src/components/App.js
@@ -13,10 +13,9 @@ class App extends Component {
 
   addGift = () => {
     const {gifts} = this.state;
-    const ids = this.state.gifts.map(gift => gift.id);
-    const max_id = max_number(ids);
 
-    gifts.push({ id: max_id + 1 });
+    gifts.push({ id: max_number(this.state.gifts.map(gift => gift.id)) + 1 });
+    
     this.setState({ gifts });
   };
 
